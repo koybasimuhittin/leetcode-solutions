575016af0809fc482e888eece7c4d32875325b46
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2023-02-21 23:43:04 +0300

    create temple.cpp

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define f1 first
+#define s2 second
+#define pb push_back
+#define mp make_pair
+#define int long long
+#define fri(a) freopen(a,"r",stdin);
+#define fro(a) freopen(a,"w",stdout);
+const int N=1e6+5;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  fri("in.txt");
+  fro("out.txt");
+}
